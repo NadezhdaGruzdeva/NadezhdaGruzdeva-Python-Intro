aa9890140b43586a638bcac881214cabd4c4c773
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-10-11 13:12:46 +0300

    changed task7

--- HW1/HW1 Task7 .py
+++ HW1/HW1 Task7 .py
@@ -3,14 +3,29 @@
 
 # 0 = FALSE
 # 1 = TRUE
+def func():
+    for x in range (2):
+        for y in range (2):
+            for z in range (2):     
+                if(not (x or y or z) == (not x and not y and not z)):
+                    continue
+                else:
+                    return print ('Утверждение не верно')
+                    # break #не знаю как сделать break из всех чисел
+    if (not(x or y or z) == (not x and not y and not z)):
+        return print ('Утверждение истино')
+func()
 
-for x in range (2):
-    for y in range (2):
-        for z in range (2):     
-            if(not (x or y or z) == (not x and not y and not z)):
-                continue
-            else:
-                print ('Утверждение не верно')
-                break #не знаю как сделать break из всех чисел
-if (not(x or y or z) == (not x and not y and not z)):
-    print ('Утверждение истино')
+# для проверики ложного
+# def func():
+#     for x in range (2):
+#         for y in range (2):
+#             for z in range (2):     
+#                 if((x or y or z) == (not x and not y and not z)):
+#                     continue
+#                 else:
+#                     return print ('Утверждение не верно')
+#                     # break #не знаю как сделать break из всех чисел
+#     if ((x or y or z) == (not x and not y and not z)):
+#         return print ('Утверждение истино')
+# func()

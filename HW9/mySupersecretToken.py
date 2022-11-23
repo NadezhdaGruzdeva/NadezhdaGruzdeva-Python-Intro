acbc4564eab44060aa898f16ed4cc7ad36b33522
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-11-23 12:21:03 +0300

    first steps to TgBot

---
+++ HW9/mySupersecretToken.py
@@ -0,0 +1,7 @@
+def Get_my_taken():
+    return '5888622101:AAFGilu8W3iJy7MWk7sVU8459rAofGntUcQ'
+
+if __name__ == "__main__": # this construction allows launch following code ontly from this file
+    print(Get_my_taken())
+    print(type(Get_my_taken()))
+    

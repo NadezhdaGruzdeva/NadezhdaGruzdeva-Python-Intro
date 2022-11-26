2149ec1bdfce32cc9f12bb679f450218b7413452
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-11-26 19:15:04 +0300

    bags fixed

--- HW10/telebotLikeLect.py
+++ HW10/telebotLikeLect.py
@@ -16,6 +16,7 @@ def help_command(update: Update, context: CallbackContext):
     update.message.reply_text(f"Hi {update.effective_user.first_name}!"\
     "\n I can make some calculations for you."\
     "\nJust type /calcInt 2 + 1 if you want to make calculation with integer numbers. \n"\
+    "\nType /calcR 2.1 + 1.3 if you want to make calculation with rational numbers. \n"\
     "\n As well I can make calculations with complex numbers."\
     "\nJust type /calcC (1+1j) + (1+1j) \n")
 
@@ -27,6 +28,24 @@ def CalculatorInt(update: Update, context: CallbackContext):
     x = int(context.args[0])
     action = context.args[1]
     y = int(context.args[2])
+
+    if action == "+":
+        update.message.reply_text(f'{x} {action} {y} = {x + y}')
+    elif action == "-":
+        update.message.reply_text(f'{x} {action} {y} = {x - y}')
+    elif action == "*":
+        update.message.reply_text(f'{x} {action} {y} = {x * y}')
+    elif action == "/":
+        update.message.reply_text(f'{x} {action} {y} = {x / y}')
+
+def CalculatorRat(update: Update, context: CallbackContext):
+    log(update, context)
+    msg = update.message.text
+    print(msg)
+
+    x = float(context.args[0])
+    action = context.args[1]
+    y = float(context.args[2])
 
     if action == "+":
         update.message.reply_text(f'{x} {action} {y} = {x + y}')
@@ -63,8 +82,9 @@ updater = Updater(Get_my_Tg_taken())
 def startCommand(update: Update, context: CallbackContext):
     buttonA = telegram.InlineKeyboardButton('help', callback_data='buttonA')
     buttonB = telegram.InlineKeyboardButton('calculator integer', callback_data='buttonB')
-    buttonC = telegram.InlineKeyboardButton('calculator complex', callback_data='buttonC')
-    markup = telegram.InlineKeyboardMarkup(inline_keyboard=[[buttonA], [buttonB], [buttonC]])
+    buttonC = telegram.InlineKeyboardButton('calculator rational', callback_data='buttonC')
+    buttonD = telegram.InlineKeyboardButton('calculator complex', callback_data='buttonD')
+    markup = telegram.InlineKeyboardMarkup(inline_keyboard=[[buttonA], [buttonB], [buttonC], [buttonD]])
 
     update.message.reply_text('Добрый день! Чтобы начать работу, выберите одно из возможных действий',
                               reply_markup=markup)
@@ -82,20 +102,27 @@ def callback(update: Update, context: CallbackContext):
         query.answer()
         query.edit_message_text(text='Хотите произвести арифметическую операцию c целыми числами?'\
             '\nВведите /calcInt число1 операция число2. \n Например: /calcInt 2 + 1'\
-            '\n Операцию выделите пробелом с двух сторон')
-
+            '\nОперацию выделите пробелом с двух сторон')
+    
     if variant == 'buttonC':
+        query.answer()
+        query.edit_message_text(text='Хотите произвести арифметическую операцию c рациональными числами?'\
+            '\nВведите /calcR число1 операция число2. \n Например: /calcInt 2.2 + 1.1'\
+            '\nОперацию выделите пробелом с двух сторон')
+
+    if variant == 'buttonD':
         query.answer()
         query.edit_message_text(text='Хотите произвести арифметическую операцию c комплексными числами?'\
             '\nВведите /calcC число1 операция число2. \nНапример: /calcC (1+1j) + (1+1j)'\
-            '\n Комлексные числа записывайте в скобках без пробелов.'\
-            '\n Операцию над ними выделите пробелом с двух сторон')
+            '\nКомлексные числа записывайте в скобках без пробелов.'\
+            '\nОперацию над ними выделите пробелом с двух сторон')
 
 
 updater.dispatcher.add_handler(CommandHandler('help', help_command))
 updater.dispatcher.add_handler(CommandHandler('start', startCommand))
 updater.dispatcher.add_handler(CommandHandler('calcInt', CalculatorInt))
 updater.dispatcher.add_handler(CommandHandler('calcC', CalculatorComplex))
+updater.dispatcher.add_handler(CommandHandler('calcR', CalculatorRat))
 updater.dispatcher.add_handler(CallbackQueryHandler(callback=callback, pattern=None, run_async=False)
 )
 
